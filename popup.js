3bf684cb1b605be1842876a23369d5073adf7c5f
jonnallex <alex.mishko12@gmail.com>
2023-08-24 15:47:49 +0300

    fix

--- popup.js
+++ popup.js
@@ -1,5 +1,4 @@
-document.getElementById('play_sound_btn').addEventListener('click', function() {
-   const audio = new Audio('sound.ogg');
-   audio.play();
-      // window.postMessage({ type: "FROM_EXTENSION", data: "Hello from the extension!" }, "*");
+window.addEventListener("DOMContentLoaded", (event) => {
+      const audio = new Audio('sound.ogg');
+      audio.play();
 });
